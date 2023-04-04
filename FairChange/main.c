c3230170c88e8d0c45b3396c5525246c266d66c9
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-04-04 22:56:02 +0530

    Possible or Not DONE.

--- FairChange/main.c
+++ FairChange/main.c
@@ -1,48 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int isPossible(int amount, int i, int coins[], int count[], int **dp)
+int isAmountPossible(int amount, int i, int *coins, int *counts, int **dp)
 {
+    if (amount < 0 || i < 0 || counts[i] < 0)
+    {
+        return 0;
+    }
     if (!amount)
     {
         return 1;
     }
-    if (!i)
+    if (dp[i][amount] != -1)
     {
-        return (!(amount % coins[i]) && (amount / coins[i] <= count[i])) ? 1 : 0;
+        return dp[i][amount];
     }
-    if (dp[amount][i] != -1)
-    {
-        return dp[amount][i];
-    }
-    int n = (amount / coins[i] > count[i]) ? count[i] : amount / coins[i];
-    dp[amount][i] = isPossible(amount - coins[i] * n, i - 1, coins, count, dp);
-    return dp[amount][i];
+    counts[i]--;
+    int take = isAmountPossible(amount - coins[i], i, coins, counts, dp);
+    counts[i]++;
+    int notTake = isAmountPossible(amount, i - 1, coins, counts, dp);
+    return dp[i][amount] = take || notTake;
 }
 
 int main()
 {
-    int amount = 50 - 17, n = 5;
-    int coins[] = {1, 2, 5, 10, 20}, count[] = {4, 2, 2, 2, 1};
+    printf("How many different coins are present? ");
+    int n;
+    scanf("%d", &n);
 
-    int **dp = (int **)malloc(sizeof(int *) * (amount + 1));
-    for (int i = 0; i < amount + 1; i++)
+    int *coins = (int *)malloc(sizeof(int) * n);
+    int *counts = (int *)malloc(sizeof(int) * n);
+    printf("What are them and their frequency?\n");
+    for (int i = 0; i < n; i++)
     {
-        dp[i] = (int *)malloc(sizeof(int) * n);
-        for (int j = 0; j < n; j++)
+        scanf("%d", coins + i);
+        scanf("%d", counts + i);
+    }
+
+    printf("What is the amount? ");
+    int amount;
+    scanf("%d", &amount);
+
+    int **dp = (int **)malloc(sizeof(int *) * n);
+    for (int i = 0; i < n; i++)
+    {
+        dp[i] = (int *)malloc(sizeof(int) * (amount + 1));
+        for (int j = 0; j < amount + 1; j++)
         {
             dp[i][j] = -1;
         }
     }
 
-    if (isPossible(amount, n - 1, coins, count, dp))
-    {
-        printf("possible\n");
-    }
-    else
-    {
-        printf("not possible\n");
-    }
+    printf("The amount can%s be given\n", isAmountPossible(amount, n - 1, coins, counts, dp) ? "" : "not");
 
     return 0;
 }
